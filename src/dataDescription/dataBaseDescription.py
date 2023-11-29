c4ced7dc7de5177ca21f785224d0349fe086c3cd
DianeLantrann <diane.lantran@gmail.com>
2023-11-29 14:32:17 -0500

    ajout graphes de correlation

--- src/dataDescription/dataBaseDescription.py
+++ src/dataDescription/dataBaseDescription.py
@@ -7,12 +7,23 @@ df = pd.read_csv(filename, delimiter = "\t")
 
 description = df.describe()
 
-# Dépenses moyennes par catégories
-graph.boxPlotAmounts(df)
-graph.meanAmountSpentByAge(df)
-graph.meanAmountSpentByIncome(df)
-graph.meanAmountSpentByNbKid(df)
-graph.meanAmountSpentByNbTeen(df)
-graph.meanAmountSpentByEducation(df)
-graph.meanAmountSpentByMaritalStatus(df)
-graph.meanAmountSpentByDtCustomer(df)
+# DEPENSE MOYENNES PAR CATEGORIE
+graph.boxPlotAmounts(df) 
+graph.meanAmountSpentByAge(df) #motif parabolique pour le vin et la viande avec des plus faibles dépenses pour les personnes nées au début des années 80
+graph.meanAmountSpentByIncome(df) #a mesure que le revenu croit les depenses en viande/vin augmentent relation visiblement lineaire
+graph.meanAmountSpentByNbKid(df) #des que des enfants sont présents dans le foyer les montants consacrés au vin chutent
+graph.meanAmountSpentByNbTeen(df) #pas de corrélation visible
+graph.meanAmountSpentByEducation(df) #relation linéaire visible entre produits de luxe (vin/viande) et niveau d'étude
+graph.meanAmountSpentByMaritalStatus(df) #pas de corrélation visible
+graph.meanAmountSpentByDtCustomer(df) #pics de dépense réguliers observés sur les débuts/fins de mois
+graph.meanAmountSpentByRecency(df) #pas de correlation apparente
+
+# MESURE DE LA CORRELATION
+graph.meanAmountBirthYScatter(df)
+graph.meanAmountIncome(df)
+graph.meanAmountNbKid(df)
+graph.meanAmountNbTeen(df)
+graph.meanAmountEducation(df)
+graph.meanAmountMaritalStatus(df)
+graph.meanAmountDtCustomer(df)
+graph.meanAmountRecency(df)
--- src/dataDescription/graphsUtils.py
+++ src/dataDescription/graphsUtils.py
@@ -7,6 +7,12 @@ Created on Wed Nov 29 08:4:09 2023
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.stats import pearsonr
+
+
+#######################################################################
+####VISUALISATION DES DEPENSES MOYENNES EN FONCTION DU PROFIL CLIENT###
+#######################################################################
 
 def boxPlotAmounts(df):
     #crée le diagramme moustache pour tous les types de dépenses de la base de données
@@ -217,7 +223,6 @@ def meanAmountSpentByMaritalStatus(df):
     plt.savefig('graphs_BDD/meanAmountByMarritalStatus.png')
     plt.show()
 
-
 def meanAmountSpentByDtCustomer(df):
     df['Dt_Customer'] = pd.to_datetime(df['Dt_Customer'], format='%d-%m-%Y')
 
@@ -251,4 +256,312 @@ def meanAmountSpentByDtCustomer(df):
     plt.title('Montant moyen dépensé par catégorie de dépense et par date de souscription')
     plt.legend()
     plt.savefig('graphs_BDD/meanAmountByCategoryAndDate.png')
+    plt.show()
+
+def meanAmountSpentByRecency(df):
+    #crée le diagramme bar montrant le montant moyen dépensé pour chaque catégorie de dépense et par nombre d'adolescent dans le foyer
+    grouped_wines = df.groupby('Recency')['MntWines'].mean().reset_index()
+    grouped_fruits = df.groupby('Recency')['MntFruits'].mean().reset_index()
+    grouped_meat = df.groupby('Recency')['MntMeatProducts'].mean().reset_index()
+    grouped_fish = df.groupby('Recency')['MntFishProducts'].mean().reset_index()
+    grouped_sweet = df.groupby('Recency')['MntSweetProducts'].mean().reset_index()
+    grouped_gold = df.groupby('Recency')['MntGoldProds'].mean().reset_index()
+
+    # Fusionne les données
+    merged_df = pd.merge(grouped_wines, grouped_fruits, on='Recency')
+    merged_df = pd.merge(merged_df, grouped_meat, on='Recency')
+    merged_df = pd.merge(merged_df, grouped_fish, on='Recency')
+    merged_df = pd.merge(merged_df, grouped_sweet, on='Recency')
+    merged_df = pd.merge(merged_df, grouped_gold, on='Recency')
+
+    # Affichage
+    width = 0.15
+    nbTeen_values = merged_df['Recency']
+    categories = ['Wines', 'Fruits', 'MeatProducts', 'FishProducts', 'SweetProducts', 'GoldProds']
+    for i, category in enumerate(categories):
+        plt.bar(nbTeen_values + i * width, merged_df[f'Mnt{category}'], width=width, label=category)
+    plt.xlabel("Frequence d'achat")
+    plt.ylabel('Montant moyen dépensé')
+    plt.title("Montant moyen dépensé par catégorie de dépense et par frequence d'achat")
+    plt.legend()
+    plt.savefig('graphs_BDD/meanAmountByRecency.png')
+    plt.show()
+
+##########################################
+###CALCUL DU COEFFICIENT DE CORRELATION###
+##########################################
+
+def meanAmountBirthYScatter(df):
+    #scatter le montant moyen dépensé pour chaque catégorie de dépense en fonction de l'année de naissance du client
+    grouped_wines = df.groupby('Year_Birth')['MntWines'].mean().reset_index()
+    grouped_fruits = df.groupby('Year_Birth')['MntFruits'].mean().reset_index()
+    grouped_meat = df.groupby('Year_Birth')['MntMeatProducts'].mean().reset_index()
+    grouped_fish = df.groupby('Year_Birth')['MntFishProducts'].mean().reset_index()
+    grouped_sweet = df.groupby('Year_Birth')['MntSweetProducts'].mean().reset_index()
+    grouped_gold = df.groupby('Year_Birth')['MntGoldProds'].mean().reset_index()
+
+    # Fusionne les données
+    merged_df = pd.merge(grouped_wines, grouped_fruits, on='Year_Birth')
+    merged_df = pd.merge(merged_df, grouped_meat, on='Year_Birth')
+    merged_df = pd.merge(merged_df, grouped_fish, on='Year_Birth')
+    merged_df = pd.merge(merged_df, grouped_sweet, on='Year_Birth')
+    merged_df = pd.merge(merged_df, grouped_gold, on='Year_Birth')
+
+    categories = ['Wines', 'Fruits', 'MeatProducts', 'FishProducts', 'SweetProducts', 'GoldProds']
+    plt.figure(figsize=(12, 8))
+    for i, category in enumerate(categories):
+        plt.scatter(merged_df['Year_Birth'], merged_df[f'Mnt{category}'], label=category)
+    plt.title('Depenses moyennes par catégorie et par année de naissance')
+    plt.xlabel("Année de naissance")
+    plt.ylabel("Dépenses moyenne par catégorie")
+    plt.xticks(rotation=45, ha='right')
+    for i, category in enumerate(categories):
+        correlation_coefficient, _ = pearsonr(merged_df['Year_Birth'], merged_df[f'Mnt{category}'])
+        plt.text(0.8, 0.9 - 0.1*i, f"{category} - Correlation Coefficient: {correlation_coefficient:.2f}",
+                 horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes, fontsize=12)
+    plt.savefig('graphs_BDD/correlationBirthYearExpenses.png')
+    plt.show()
+
+def meanAmountIncome(df):
+    #scatter le montant moyen dépensé pour chaque catégorie de dépense en fonction du revenu
+    grouped_wines = df.groupby('Income')['MntWines'].mean().reset_index()
+    grouped_fruits = df.groupby('Income')['MntFruits'].mean().reset_index()
+    grouped_meat = df.groupby('Income')['MntMeatProducts'].mean().reset_index()
+    grouped_fish = df.groupby('Income')['MntFishProducts'].mean().reset_index()
+    grouped_sweet = df.groupby('Income')['MntSweetProducts'].mean().reset_index()
+    grouped_gold = df.groupby('Income')['MntGoldProds'].mean().reset_index()
+
+    # Fusionne les données
+    merged_df = pd.merge(grouped_wines, grouped_fruits, on='Income')
+    merged_df = pd.merge(merged_df, grouped_meat, on='Income')
+    merged_df = pd.merge(merged_df, grouped_fish, on='Income')
+    merged_df = pd.merge(merged_df, grouped_sweet, on='Income')
+    merged_df = pd.merge(merged_df, grouped_gold, on='Income')
+
+    categories = ['Wines', 'Fruits', 'MeatProducts', 'FishProducts', 'SweetProducts', 'GoldProds']
+    plt.figure(figsize=(12, 8))
+    for i, category in enumerate(categories):
+        plt.scatter(merged_df['Income'], merged_df[f'Mnt{category}'], label=category)
+    plt.title('Depenses moyennes par catégorie et par revenu annuel')
+    plt.xlabel("Revenu annuel")
+    plt.ylabel("Dépenses moyenne par catégorie")
+    plt.xticks(rotation=45, ha='right')
+    for i, category in enumerate(categories):
+        correlation_coefficient, _ = pearsonr(merged_df['Income'], merged_df[f'Mnt{category}'])
+        plt.text(0.8, 0.9 - 0.1*i, f"{category} - Correlation Coefficient: {correlation_coefficient:.2f}",
+                 horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes, fontsize=12)
+    plt.savefig('graphs_BDD/correlationIncomExpenses.png')
+    plt.show()
+
+def meanAmountNbKid(df):
+    # Scatter plot of the average amount spent for each expense category based on the number of kids
+    grouped_wines = df.groupby('Kidhome')['MntWines'].mean().reset_index()
+    grouped_fruits = df.groupby('Kidhome')['MntFruits'].mean().reset_index()
+    grouped_meat = df.groupby('Kidhome')['MntMeatProducts'].mean().reset_index()
+    grouped_fish = df.groupby('Kidhome')['MntFishProducts'].mean().reset_index()
+    grouped_sweet = df.groupby('Kidhome')['MntSweetProducts'].mean().reset_index()
+    grouped_gold = df.groupby('Kidhome')['MntGoldProds'].mean().reset_index()
+
+    # Merge the data
+    merged_df = pd.merge(grouped_wines, grouped_fruits, on='Kidhome')
+    merged_df = pd.merge(merged_df, grouped_meat, on='Kidhome')
+    merged_df = pd.merge(merged_df, grouped_fish, on='Kidhome')
+    merged_df = pd.merge(merged_df, grouped_sweet, on='Kidhome')
+    merged_df = pd.merge(merged_df, grouped_gold, on='Kidhome')
+
+    categories = ['Wines', 'Fruits', 'MeatProducts', 'FishProducts', 'SweetProducts', 'GoldProds']
+    plt.figure(figsize=(12, 8))
+    for i, category in enumerate(categories):
+        plt.scatter(merged_df['Kidhome'], merged_df[f'Mnt{category}'], label=category)
+
+    plt.title("Dépenses moyennes par nombre d'enfants dans le foyer")
+    plt.xlabel("Nombre d'enfants")
+    plt.ylabel("Dépenses moyennes par catégorie")
+    
+    # Set integer ticks on the x-axis
+    plt.xticks(list(map(int, merged_df['Kidhome'])))
+    
+    plt.xticks(rotation=45, ha='right')
+    
+    for i, category in enumerate(categories):
+        correlation_coefficient, _ = pearsonr(merged_df['Kidhome'], merged_df[f'Mnt{category}'])
+        plt.text(0.8, 0.9 - 0.1*i, f"{category} - Correlation Coefficient: {correlation_coefficient:.2f}",
+                 horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes, fontsize=12)
+
+    plt.savefig('graphs_BDD/correlationNbKidExpenses.png')
+    plt.show()
+
+def meanAmountNbTeen(df):
+    # Scatter plot of the average amount spent for each expense category based on the number of teenagers
+    grouped_wines = df.groupby('Teenhome')['MntWines'].mean().reset_index()
+    grouped_fruits = df.groupby('Teenhome')['MntFruits'].mean().reset_index()
+    grouped_meat = df.groupby('Teenhome')['MntMeatProducts'].mean().reset_index()
+    grouped_fish = df.groupby('Teenhome')['MntFishProducts'].mean().reset_index()
+    grouped_sweet = df.groupby('Teenhome')['MntSweetProducts'].mean().reset_index()
+    grouped_gold = df.groupby('Teenhome')['MntGoldProds'].mean().reset_index()
+    # Merge the data
+    merged_df = pd.merge(grouped_wines, grouped_fruits, on='Teenhome')
+    merged_df = pd.merge(merged_df, grouped_meat, on='Teenhome')
+    merged_df = pd.merge(merged_df, grouped_fish, on='Teenhome')
+    merged_df = pd.merge(merged_df, grouped_sweet, on='Teenhome')
+    merged_df = pd.merge(merged_df, grouped_gold, on='Teenhome')
+    categories = ['Wines', 'Fruits', 'MeatProducts', 'FishProducts', 'SweetProducts', 'GoldProds']
+    plt.figure(figsize=(12, 8))
+    for i, category in enumerate(categories):
+        plt.scatter(merged_df['Teenhome'], merged_df[f'Mnt{category}'], label=category)
+    plt.title("Dépenses moyennes par nombre d'adolescents dans le foyer")
+    plt.xlabel("Nombre d'adolescents")
+    plt.ylabel("Dépenses moyennes par catégorie")
+    
+    # Set integer ticks on the x-axis
+    plt.xticks(list(map(int, merged_df['Teenhome'])))
+    plt.xticks(rotation=45, ha='right')
+    for i, category in enumerate(categories):
+        correlation_coefficient, _ = pearsonr(merged_df['Teenhome'], merged_df[f'Mnt{category}'])
+        plt.text(0.8, 0.9 - 0.1*i, f"{category} - Correlation Coefficient: {correlation_coefficient:.2f}",
+                 horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes, fontsize=12)
+    plt.savefig('graphs_BDD/correlationNbTeenExpenses.png')
+    plt.show()
+
+def meanAmountEducation(df):
+    # Scatter plot of the average amount spent for each expense category based on the level of education
+    grouped_wines = df.groupby('Education')['MntWines'].mean().reset_index()
+    grouped_fruits = df.groupby('Education')['MntFruits'].mean().reset_index()
+    grouped_meat = df.groupby('Education')['MntMeatProducts'].mean().reset_index()
+    grouped_fish = df.groupby('Education')['MntFishProducts'].mean().reset_index()
+    grouped_sweet = df.groupby('Education')['MntSweetProducts'].mean().reset_index()
+    grouped_gold = df.groupby('Education')['MntGoldProds'].mean().reset_index()
+
+    # Merge the data
+    merged_df = pd.merge(grouped_wines, grouped_fruits, on='Education')
+    merged_df = pd.merge(merged_df, grouped_meat, on='Education')
+    merged_df = pd.merge(merged_df, grouped_fish, on='Education')
+    merged_df = pd.merge(merged_df, grouped_sweet, on='Education')
+    merged_df = pd.merge(merged_df, grouped_gold, on='Education')
+
+    categories = ['Wines', 'Fruits', 'MeatProducts', 'FishProducts', 'SweetProducts', 'GoldProds']
+    plt.figure(figsize=(12, 8))
+    for i, category in enumerate(categories):
+        plt.scatter(range(len(merged_df['Education'])), merged_df[f'Mnt{category}'], label=category)
+
+    plt.title("Dépenses moyennes par niveau d'éducation")
+    plt.xlabel("Niveau d'éducation")
+    plt.ylabel("Dépenses moyennes par catégorie")
+    
+    # Set x-axis ticks based on the index of unique values
+    plt.xticks(range(len(merged_df['Education'])), merged_df['Education'], rotation=45, ha='right')
+    
+    for i, category in enumerate(categories):
+        correlation_coefficient, _ = pearsonr(range(len(merged_df['Education'])), merged_df[f'Mnt{category}'])
+        plt.text(0.8, 0.9 - 0.1*i, f"{category} - Correlation Coefficient: {correlation_coefficient:.2f}",
+                 horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes, fontsize=12)
+
+    plt.savefig('graphs_BDD/correlationEducationExpenses.png')
+    plt.show()
+
+def meanAmountMaritalStatus(df):
+    # Scatter plot of the average amount spent for each expense category based on marital status
+    grouped_wines = df.groupby('Marital_Status')['MntWines'].mean().reset_index()
+    grouped_fruits = df.groupby('Marital_Status')['MntFruits'].mean().reset_index()
+    grouped_meat = df.groupby('Marital_Status')['MntMeatProducts'].mean().reset_index()
+    grouped_fish = df.groupby('Marital_Status')['MntFishProducts'].mean().reset_index()
+    grouped_sweet = df.groupby('Marital_Status')['MntSweetProducts'].mean().reset_index()
+    grouped_gold = df.groupby('Marital_Status')['MntGoldProds'].mean().reset_index()
+
+    # Merge the data
+    merged_df = pd.merge(grouped_wines, grouped_fruits, on='Marital_Status')
+    merged_df = pd.merge(merged_df, grouped_meat, on='Marital_Status')
+    merged_df = pd.merge(merged_df, grouped_fish, on='Marital_Status')
+    merged_df = pd.merge(merged_df, grouped_sweet, on='Marital_Status')
+    merged_df = pd.merge(merged_df, grouped_gold, on='Marital_Status')
+
+    categories = ['Wines', 'Fruits', 'MeatProducts', 'FishProducts', 'SweetProducts', 'GoldProds']
+    plt.figure(figsize=(12, 8))
+    for i, category in enumerate(categories):
+        plt.scatter(range(len(merged_df['Marital_Status'])), merged_df[f'Mnt{category}'], label=category)
+
+    plt.title("Dépenses moyennes par statut marital")
+    plt.xlabel("Statut marital")
+    plt.ylabel("Dépenses moyennes par catégorie")
+    
+    # Set x-axis ticks based on the index of unique values
+    plt.xticks(range(len(merged_df['Marital_Status'])), merged_df['Marital_Status'], rotation=45, ha='right')
+    
+    for i, category in enumerate(categories):
+        correlation_coefficient, _ = pearsonr(range(len(merged_df['Marital_Status'])), merged_df[f'Mnt{category}'])
+        plt.text(0.8, 0.9 - 0.1*i, f"{category} - Correlation Coefficient: {correlation_coefficient:.2f}",
+                 horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes, fontsize=12)
+
+    plt.savefig('graphs_BDD/correlationMarital_StatusExpenses.png')
+    plt.show()
+
+def meanAmountDtCustomer(df):
+    # Convert 'Dt_Customer' column to datetime objects
+    df['Dt_Customer'] = pd.to_datetime(df['Dt_Customer'], format='%d-%m-%Y')
+
+    # Scatter plot of the average amount spent for each expense category based on 'Dt_Customer'
+    grouped_wines = df.groupby('Dt_Customer')['MntWines'].mean().reset_index()
+    grouped_fruits = df.groupby('Dt_Customer')['MntFruits'].mean().reset_index()
+    grouped_meat = df.groupby('Dt_Customer')['MntMeatProducts'].mean().reset_index()
+    grouped_fish = df.groupby('Dt_Customer')['MntFishProducts'].mean().reset_index()
+    grouped_sweet = df.groupby('Dt_Customer')['MntSweetProducts'].mean().reset_index()
+    grouped_gold = df.groupby('Dt_Customer')['MntGoldProds'].mean().reset_index()
+
+    # Merge the data
+    merged_df = pd.merge(grouped_wines, grouped_fruits, on='Dt_Customer')
+    merged_df = pd.merge(merged_df, grouped_meat, on='Dt_Customer')
+    merged_df = pd.merge(merged_df, grouped_fish, on='Dt_Customer')
+    merged_df = pd.merge(merged_df, grouped_sweet, on='Dt_Customer')
+    merged_df = pd.merge(merged_df, grouped_gold, on='Dt_Customer')
+
+    categories = ['Wines', 'Fruits', 'MeatProducts', 'FishProducts', 'SweetProducts', 'GoldProds']
+    plt.figure(figsize=(12, 8))
+    for i, category in enumerate(categories):
+        # Convert datetime to Unix timestamp
+        timestamp = merged_df['Dt_Customer'].astype('int64') // 10**9  # Convert nanoseconds to seconds
+        plt.scatter(timestamp, merged_df[f'Mnt{category}'], label=category)
+
+    plt.title("Dépenses moyennes par date")
+    plt.xlabel("Date d'inscription")
+    plt.ylabel("Dépenses moyennes par catégorie")
+    plt.xticks(rotation=45, ha='right')
+
+    for i, category in enumerate(categories):
+        correlation_coefficient, _ = pearsonr(timestamp, merged_df[f'Mnt{category}'])
+        plt.text(0.8, 0.9 - 0.1*i, f"{category} - Correlation Coefficient: {correlation_coefficient:.2f}",
+                 horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes, fontsize=12)
+
+    plt.savefig('graphs_BDD/correlationDt_CustomerExpenses.png')
+    plt.show()
+
+
+def meanAmountRecency(df):
+    #scatter le montant moyen dépensé pour chaque catégorie de dépense en fonction de la fréquence d'achat
+    grouped_wines = df.groupby('Recency')['MntWines'].mean().reset_index()
+    grouped_fruits = df.groupby('Recency')['MntFruits'].mean().reset_index()
+    grouped_meat = df.groupby('Recency')['MntMeatProducts'].mean().reset_index()
+    grouped_fish = df.groupby('Recency')['MntFishProducts'].mean().reset_index()
+    grouped_sweet = df.groupby('Recency')['MntSweetProducts'].mean().reset_index()
+    grouped_gold = df.groupby('Recency')['MntGoldProds'].mean().reset_index()
+
+    # Fusionne les données
+    merged_df = pd.merge(grouped_wines, grouped_fruits, on='Recency')
+    merged_df = pd.merge(merged_df, grouped_meat, on='Recency')
+    merged_df = pd.merge(merged_df, grouped_fish, on='Recency')
+    merged_df = pd.merge(merged_df, grouped_sweet, on='Recency')
+    merged_df = pd.merge(merged_df, grouped_gold, on='Recency')
+
+    categories = ['Wines', 'Fruits', 'MeatProducts', 'FishProducts', 'SweetProducts', 'GoldProds']
+    plt.figure(figsize=(12, 8))
+    for i, category in enumerate(categories):
+        plt.scatter(merged_df['Recency'], merged_df[f'Mnt{category}'], label=category)
+    plt.title("Depenses moyennes par catégorie et par frequence d'achat")
+    plt.xlabel("Fréquence d'achat")
+    plt.ylabel("Dépenses moyenne par catégorie")
+    plt.xticks(rotation=45, ha='right')
+    for i, category in enumerate(categories):
+        correlation_coefficient, _ = pearsonr(merged_df['Recency'], merged_df[f'Mnt{category}'])
+        plt.text(0.8, 0.9 - 0.1*i, f"{category} - Correlation Coefficient: {correlation_coefficient:.2f}",
+                 horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes, fontsize=12)
+    plt.savefig('graphs_BDD/correlationRecencyExpenses.png')
     plt.show()
